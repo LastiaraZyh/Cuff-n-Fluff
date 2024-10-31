38852bb65a9c27d58b05d17e88f77e9d3ff05cd2
go49peb <lastiara.zou@tum.de>
2024-10-31 11:16:53 +0100

    https://go49peb@artemis.cit.tum.de/git/FOPHN2425T02E01/fophn2425t02e01-go49peb.git

--- src/de/tum/cit/fop/Penguin.java
+++ src/de/tum/cit/fop/Penguin.java
@@ -44,7 +44,36 @@ public class Penguin {
         this.choice = choice;
     }
 }
+class Interrogator {
+    private String name;
+    private String tactic;
 
+    public static final String OFFER_DEAL = "O";
+    public static final String THREATEN = "T";
+
+    // 构造器，只接受 name 参数
+    public Interrogator(String name) {
+        this.name = name;
+        this.tactic = "NONE";
+    }
+
+    // Getter 和 Setter 方法
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {  // 添加 setName 方法
+        this.name = name;
+    }
+
+    public String getTactic() {
+        return tactic;
+    }
+
+    public void setTactic(String tactic) {
+        this.tactic = tactic;
+    }
+}
 class Interrogator {
     private String name;
     private String tactic;
